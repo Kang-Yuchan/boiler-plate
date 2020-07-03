e2e29aa46fde241ee483bb2cb38f5fa4935ffee8
Yuchan <mikuplayer123@gmail.com>
2020-07-03 17:28:28 +0900

    connect mongo db with mongoose

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    `mongodb+srv://kangyuchan:${DB_PASSWORD}@cluster0.g9bv5.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => console.log("MongoDB connected.."))
+  .catch(error => console.log(error));
 
 app.get("/", (req, res) => res.send("Hello express"));
 
