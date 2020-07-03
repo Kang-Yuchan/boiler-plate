7345837f725865419b0c8783ca5d29b0cc68c51b
Yuchan <mikuplayer123@gmail.com>
2020-07-03 17:57:31 +0900

    user register

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
 dotenv.config();
 
 const PORT = process.env.PORT;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-const mongoose = require("mongoose");
 mongoose
   .connect(
     `mongodb+srv://kangyuchan:${DB_PASSWORD}@cluster0.g9bv5.mongodb.net/test?retryWrites=true&w=majority`,
@@ -20,7 +22,23 @@ mongoose
   .then(() => console.log("MongoDB connected.."))
   .catch(error => console.log(error));
 
+app.use(bodyParser.urlencoded({ extended: true })); // application/x-www-form-urlencoded
+app.use(bodyParser.json()); // application/json
+
 app.get("/", (req, res) => res.send("Hello express"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({
+      success: true
+    });
+  });
+});
 
 app.listen(PORT, () =>
   PORT
