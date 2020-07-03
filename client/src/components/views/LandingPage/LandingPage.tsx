7abe66d9f08190442a369c5a1c04f382a0e3b64d
Yuchan <mikuplayer123@gmail.com>
2020-07-04 00:11:40 +0900

    CORS

--- client/src/components/views/LandingPage/LandingPage.tsx
+++ client/src/components/views/LandingPage/LandingPage.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
+import Axios from "axios";
 
 const LandingPage: React.FC = () => {
+  React.useEffect(() => {
+    Axios.get("http://localhost:8080/api/hello").then(response =>
+      console.log(response)
+    );
+  }, []);
   return <div className='LandingPage'>LandingPage</div>;
 };
 
